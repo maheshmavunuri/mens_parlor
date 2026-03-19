f5f3641224553a28cd11e596b18fd0c06888eef1
maheshmavunuri <maheshmavunuri77058@gmail.com>
2026-03-19 19:27:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ init_db()
 # -----------------------------
 @app.route('/')
 def home():
-    return render_template('index.html')
-
+    return "WORKING"
 # -----------------------------
 # BOOKING PAGE
 # -----------------------------
